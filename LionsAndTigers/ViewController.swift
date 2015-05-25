b06575fce034f7887a1cfdc7b7e9337872a14ad4
Richard Hubbard <rchubbard@Ricks-MacBook-Pro.local>
2015-05-25 13:17:30 -0500

    Initial Commit

---
+++ LionsAndTigers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LionsAndTigers
+//
+//  Created by Richard Hubbard on 5/25/15.
+//  Copyright (c) 2015 Hubbard Consultants. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
